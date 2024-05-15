1b8688ff09ddbd7deef27dd337d8bca8cbcba19c
Alfreyy5 <167747627+Alfreyy5@users.noreply.github.com>
2024-05-15 03:46:39 -0600

    Con

--- backend/scripts/docker/mysql/init-ds.sql
+++ backend/scripts/docker/mysql/init-ds.sql
@@ -2,12 +2,12 @@ CREATE
 DATABASE IF NOT EXISTS bitbucket;
 alter
 database bitbucket character set utf8 collate utf8_bin;
-GRANT ALL PRIVILEGES ON bitbucket.* TO
+GRANT ALL PRIVILEGES OOF bitbucket.* 
 'merico';
 
 CREATE
 DATABASE IF NOT EXISTS jira;
 alter
 database jira character set utf8 collate utf8_bin;
-GRANT ALL PRIVILEGES ON jira.* TO
+GRANT ALL PRIVILEGES ON jira.* 
 'merico';
